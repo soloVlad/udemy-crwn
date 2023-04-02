bff9a552e5e709c35539b08b3bbfc098ddc345d0
Vlad Solovey <connectwithvlad@gmail.com>
2023-04-02 22:15:00 +0300

    feat: add authentication

--- src/components/sign-up-form/sign-up-form.jsx
+++ src/components/sign-up-form/sign-up-form.jsx
@@ -18,8 +18,6 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   }
@@ -59,7 +57,7 @@ const SignUpForm = () => {
         <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email} />
         <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
         <FormInput label="Confirm Password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
-        <Button type="submit">Sign UP</Button>
+        <Button type="submit">Sign In</Button>
       </form>
     </div>
   );
