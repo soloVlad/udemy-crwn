aaf3a1933660ce22f2512f09335e5a4b1df80565
Vlad Solovey <connectwithvlad@gmail.com>
2023-04-07 22:19:58 +0300

    feat: add fetching products

---
+++ src/components/contexts/products.context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useEffect, useState } from "react";
+
+import PRODUCTS from '../../shop-data.json';
+
+export const ProductsContext = createContext({
+  products: [],
+  setProducts: () => null
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const value = { products, setProducts };
+
+  useEffect(() => {
+    setProducts(PRODUCTS);
+  }, []);
+
+  return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './components/contexts/user.context';
+import { ProductsProvider } from './components/contexts/products.context';
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
@@ -14,7 +15,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
