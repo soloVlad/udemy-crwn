795975d7d8be36b94033ea1686a7649b0bda5903
Vlad Solovey <connectwithvlad@gmail.com>
2023-04-08 22:47:26 +0300

    feat: add items to cart

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,12 +1,19 @@
-import './cart-dropdown.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../contexts/cart.context';
 
 import Button from '../button/button';
+import CartItem from '../cart-item/cart-item';
+
+import './cart-dropdown.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-
+        {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
       </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
